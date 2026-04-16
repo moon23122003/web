2d9b0ca52e32e5b84e54dea9dc86d28ea3f4d4ae
moon23122003 <monuvijayalaxmi@gmail.com>
2026-04-16 23:51:47 +0530

    Delete web.py

--- web.py
+++
@@ -1,7 +0,0 @@
-#Add two no using streamlit 
-import streamlit as st
-st.title("welcome to my page")
-name=st.text_input("Enter the name")
-if name:
-       st.write(f"hello,{name}!")
-       st.success()
