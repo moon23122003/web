dec92381d194c075975dab97f533ed3ad8962dd7
moon23122003 <monuvijayalaxmi@gmail.com>
2026-04-16 23:57:35 +0530

    Add files via upload

---
+++ loading.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import time#core python
+
+with st.spinner("Loding...."):
+       time.sleep(5)
+with st.sidebar:
+       st.write("Show the above options")
+       
+       
+st.title("health care prediction project")
